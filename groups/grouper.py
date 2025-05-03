04d69723efbf46d8c8074024cddaf32fc2851cc6
William Redding <williamdredding@proton.me>
2025-05-03 17:27:16 +0100

    Scale up block weights in grouper.py to be as big as possible

--- groups/grouper.py
+++ groups/grouper.py
@@ -4,7 +4,8 @@ import argparse
 # Set up argument parsing
 parser = argparse.ArgumentParser(description="Grouper.")
 parser.add_argument('input_file', type=str, help='The input JSON file with group data.')
-parser.add_argument('--output_file', type=str, default='output.json', help='The output JSON file (default: output.json).')
+parser.add_argument('max_weight', type=int, help='Max weight of item')
+parser.add_argument('-o', '--output_file', type=str, default='output.json', help='The output JSON file (default: output.json).')
 args = parser.parse_args()
 
 # Open and load the input JSON file
@@ -18,8 +19,16 @@ for group in groups:
     total: int = group["total"]
     items: list[str] = group["items"]
     k = total / len(items)
+    if k > args.max_weight:
+        raise ValueError(f"Max weight exceeded: {k} > {args.max_weight}")
     for item in items:
         output[item] = k
+
+max_weight_generated = max(output.values())
+multiplier = args.max_weight // max_weight_generated
+print(multiplier)
+for key in output:
+    output[key] *= multiplier
 
 # Write the output to the specified file
 with open(args.output_file, "w+") as f:
